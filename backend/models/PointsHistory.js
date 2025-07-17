c3637e8d23332fa59713aeab29770be36cd2838a
SaH1l191 <haldankarsahil98@gmail.com>
2025-07-17 19:43:39 +0530

    leaderBoard-frontend

--- backend/models/PointsHistory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const pointsHistorySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  points: { type: Number, required: true },
-  claimedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("PointsHistory", pointsHistorySchema);
--- backend/socket/socket.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*", methods: ["GET","POST","PUT","DELETE"] } });
-
-module.exports = { io, server, app };
