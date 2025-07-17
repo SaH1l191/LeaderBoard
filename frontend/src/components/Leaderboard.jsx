ecd3f2dbd48ea4fcf43459d5b537eb290f74d4f4
SaH1l191 <haldankarsahil98@gmail.com>
2025-07-17 21:58:28 +0530

    fixed-URL

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -15,7 +15,7 @@ export default function Leaderboard() {
 
   const fetchLeaderboard = async () => {
     try {
-      const res = await api.get("https://leader-board-jade.vercel.app/api/claim/leaderboard/api/claim/leaderboard")
+      const res = await api.get("https://leaderboard-be-production.up.railway.app/api/claim/leaderboard/api/claim/leaderboard")
       console.log("logging leaderboard ",res.data)
       setLeaders(res.data)
     } catch (error) {
